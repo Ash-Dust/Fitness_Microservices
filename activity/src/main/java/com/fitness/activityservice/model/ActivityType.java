730472217e50d212b0504891a67fcddf13367ac2
Ash-Dust <just.somf@gmail.com>
2026-01-04 15:08:44 +0700

    Install and connect activityservice to mongodb successfully

---
+++ activity/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    YOGA,
+    STRENGTH_TRAINING,
+    HIIT,
+    DANCE,
+    OTHER
+}
